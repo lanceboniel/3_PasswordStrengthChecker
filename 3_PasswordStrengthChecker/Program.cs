b672b80d7f638feccf8be450cc6127c3f28a9921
Admin <Admin@EA611-08>
2025-01-20 19:58:31 +0800

    Add project files.

---
+++ 3_PasswordStrengthChecker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _3_PasswordStrengthChecker
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+           
+            bool isStrong = password.Length >= 8 &&
+                            Regex.IsMatch(password, @"[A-Z]") &&
+                            Regex.IsMatch(password, @"[a-z]") &&
+                            Regex.IsMatch(password, @"[0-9]");
+                         
+            Console.WriteLine(isStrong ? "Password is strong." : "Password is weak.");
+            Console.ReadLine();
+        }
+       
+    }
+}
+
